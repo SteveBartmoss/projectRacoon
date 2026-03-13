072d2b4001e5eb415e779ae5fda8090a36d818ee
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-13 10:23:42 -0600

    feat: Se implemento el componente para los textfields de la aplicacion

---
+++ src/ui/textField/textField.jsx
@@ -0,0 +1,9 @@
+import './textfield.css'
+
+export function TextField({textHolder,target,handleTarget}){
+    
+    return(
+        <input type="text"  className='text-field' placeholder={textHolder} value={target} onChange={handleTarget} />
+    )
+
+}
