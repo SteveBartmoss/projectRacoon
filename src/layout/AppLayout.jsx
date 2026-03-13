18340bc15c43d6c42dea66eac431ab91a63bb659
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-13 10:09:08 -0600

    feat: Se implementaron los elementos para el layout de la pagina principal

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,7 +1,16 @@
 import reactLogo from "../assets/react.svg"
 import { Column, Row } from "../ui/containers/containers"
+import { Tab } from "../ui/tab/tab"
+import { RequesLayout } from "./requestLayout"
 
 export function AppLayout(){
+
+    const frameItems = [
+        {
+            title: "Prueba",
+            content: <RequesLayout />
+        },
+    ]
     
     return (
         <>
@@ -12,6 +21,11 @@ export function AppLayout(){
                     </div>
                     <div>
                         <h1>Racoon</h1>
+                    </div>
+                </Row>
+                <Row>
+                    <div>
+                        <Tab elements={frameItems} />
                     </div>
                 </Row>
                 <Row>
