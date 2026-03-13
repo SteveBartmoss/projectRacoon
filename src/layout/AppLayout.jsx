d974bae5b6404bcb06f10b3fda4e9c8f92eee8c3
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-12 20:00:56 -0600

    feat: Se inicializo el proyecto

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,19 @@
+import reactLogo from "../assets/react.svg"
+
+export function AppLayout(){
+    
+    return (
+        <>
+            <div>
+                <div>
+                    <div>
+                        <img src={reactLogo} />
+                    </div>
+                    <div>
+                        <h1>Racoon</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
