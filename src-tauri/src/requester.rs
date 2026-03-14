0b8ad26371f5744d2c596dab9a93794ab38c976b
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-13 18:04:56 -0600

    feat: Se implemento una estructura para el manejo de la informacion, se agregan los valores de body y params para la peticion http

--- src-tauri/src/requester.rs
+++ src-tauri/src/requester.rs
@@ -1,9 +1,38 @@
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
-#[tauri::command]
-pub async fn fetch_data(url: String) -> Result<serde_json::Value, String>{
+#[derive(Serialize, Deserialize)]
+pub struct HttpRequest {
+    pub url: String,
+    pub method: String,
+    pub params: Option<std::collections::HashMap<String, String>>,
+    pub body: Option<serde_json::Value>
+}
 
-    let response = reqwest::get(&url)
+#[tauri::command]
+pub async fn fetch_data(req: HttpRequest) -> Result<serde_json::Value, String>{
+
+    let client = Client::new();
+
+    let mut request = match req.method.as_str() {
+        "GET" => client.get(&req.url),
+        "POST" => client.post(&req.url),
+        "PUT" => client.put(&req.url),
+        "PATCH" => client.patch(&req.url),
+        "DELETE" => client.delete(&req.url),
+        _ => return Err("Unsupported method".into())
+    };
+
+    if let Some(params) = req.params {
+        request = request.query(&params);
+    }
+
+    if let Some(body) = req.body {
+        request = request.json(&body);
+    }
+
+    let response = request
+        .send()
         .await
         .map_err(|e| e.to_string())?;
         
