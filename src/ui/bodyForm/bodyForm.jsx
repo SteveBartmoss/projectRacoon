4892e22ad98b6bdeb01a4ca1f790e16a4eb23e7d
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-13 19:14:47 -0600

    feat: Se implemento el componente para el parametro del body en las peticiones

--- src/ui/bodyForm/bodyForm.jsx
+++ src/ui/bodyForm/bodyForm.jsx
@@ -1,6 +1,6 @@
+import './bodyform.css'
 
-
-export function BodyForm(){
+export function BodyForm({body,setBody}){
     return (
         <>
             <textarea className='text-area' placeholder='body' value={body} onChange={(event)=>setBody(event.target.value)}></textarea>
