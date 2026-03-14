04b1c939359c2cb55a82e6db0c6772c99c72a060
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-14 10:43:25 -0600

    feat: Se implementaron los datos de la estadisticas de las respuestas

--- src/ui/responseFrame/responseFrame.jsx
+++ src/ui/responseFrame/responseFrame.jsx
@@ -1,15 +1,26 @@
 import './responseFrame.css'
 
-export function ResponseFrame({objProps}){
+export function ResponseFrame({ objProps }) {
 
 
-    return(
+    return (
         <>
-        <div className='div-container'>
-            <div className='div-response'>
-                <pre>{JSON.stringify(objProps.body, null, 2)}</pre>
+            <div className='div-container'>
+                <div className='div-status-bar'>
+                    <div>
+                        <p>status: {objProps.status}</p>
+                    </div>
+                    <div>
+                        <p>time: {objProps.time}</p>
+                    </div>
+                    <div>
+                        <p>size: {objProps.size} B</p>
+                    </div>
+                </div>
+                <div className='div-response'>
+                    <pre>{JSON.stringify(objProps.body, null, 2)}</pre>
+                </div>
             </div>
-        </div>
         </>
     )
 }
