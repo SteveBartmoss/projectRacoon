3d849cea76e1d4055bf878fe13b728420bfd4d30
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-15 14:44:41 -0600

    feat: Se implemento el componente box para una mejor separacion de los elementos de estatus bar

--- src/ui/responseFrame/responseFrame.jsx
+++ src/ui/responseFrame/responseFrame.jsx
@@ -1,6 +1,7 @@
 import { Chip } from '../chip/chip'
 import './responseFrame.css'
 import copy from "../../assets/copy.svg"
+import { Box } from '../containers/containers'
 
 export function ResponseFrame({ objProps }) {
 
@@ -18,18 +19,34 @@ export function ResponseFrame({ objProps }) {
         <>
             <div className='div-container'>
                 <div className='div-status-bar'>
-                    <div>
+                    <Box
+                        styles={{
+                            margin: ".3rem",
+                        }}
+                    >
                         <Chip text={`status: ${objProps?.status}`} type={'main'}/>
-                    </div>
-                    <div>
+                    </Box>
+                    <Box
+                        styles={{
+                            margin: ".3rem",
+                        }}
+                    >
                         <Chip text={`time: ${objProps?.time}`} type={'main'}/>
-                    </div>
-                    <div>
+                    </Box>
+                    <Box
+                        styles={{
+                            margin: ".3rem",
+                        }}
+                    >
                         <Chip text={`size: ${objProps?.size}`} type={'main'} />
-                    </div>
-                    <div>
-                        <img className="copy" onClick={handleClipBoard} src={copy} />
-                    </div>
+                    </Box>
+                    <Box
+                        styles={{
+                            margin: ".3rem",
+                        }}
+                    >
+                        <img className="btn-clipboard" onClick={handleClipBoard} src={copy} />
+                    </Box>
                 </div>
                 <div className='div-response'>
                     <pre>{JSON.stringify(objProps.body, null, 2)}</pre>
