bebf273e1747b3a405f0463c32236c8ba007ad3f
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-14 21:13:43 -0600

    feat: Se implemento el componente de los chips para la informacion de la respuesta

--- src/ui/responseFrame/responseFrame.jsx
+++ src/ui/responseFrame/responseFrame.jsx
@@ -1,3 +1,4 @@
+import { Chip } from '../chip/chip'
 import './responseFrame.css'
 
 export function ResponseFrame({ objProps }) {
@@ -8,13 +9,13 @@ export function ResponseFrame({ objProps }) {
             <div className='div-container'>
                 <div className='div-status-bar'>
                     <div>
-                        <p>status: {objProps.status}</p>
+                        <Chip text={`status: ${objProps?.status}`} type={'main'}/>
                     </div>
                     <div>
-                        <p>time: {objProps.time}</p>
+                        <Chip text={`time: ${objProps?.time}`} type={'main'}/>
                     </div>
                     <div>
-                        <p>size: {objProps.size} B</p>
+                        <Chip text={`size: ${objProps?.size}`} type={'main'} />
                     </div>
                 </div>
                 <div className='div-response'>
