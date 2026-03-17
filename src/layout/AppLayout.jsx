82df990a0c7115436d0871fb90aea3e35acb5af2
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-16 22:01:28 -0600

    feat: Se removieron importaciones que no son necesarias

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,15 +1,12 @@
 import { useSelector } from "react-redux"
-import logo from "../assets/racoon.png"
 import { Column, Row } from "../ui/containers/containers"
 import { Header } from "../ui/header/header"
-import { Tab } from "../ui/tab/tab"
-import { RequesLayout } from "./requestLayout"
 import { FrameTabs } from "../ui/frameTabs/frameTabs"
 
 export function AppLayout(){
 
     const frameItems = useSelector((state) => state.frames.listFrames)
-    
+
     return (
         <>
             <Column>
