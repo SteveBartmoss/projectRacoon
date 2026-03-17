572b158defca0690d751805b4da09a09b3a7fbf6
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-16 22:03:17 -0600

    feat: Se implementaron las funciones para agregar mas tabs, seleccionar una especifico y tambien para remover una tab

--- src/ui/frameTabs/frameTabs.jsx
+++ src/ui/frameTabs/frameTabs.jsx
@@ -1,24 +1,65 @@
 import { RequesLayout } from "../../layout/requestLayout";
+import addImg from '../../assets/add.svg'
+import closeImg from '../../assets/close.svg'
+import { useDispatch, useSelector } from "react-redux";
+import { addFrame, removeFrame, setCurrentTab } from "../../store/frameSlice";
+
+export function FrameTabs({ elements }) {
+
+    const listFrames = useSelector((state) => state.frames.listFrames)
+    const tabSelected = useSelector((state) => state.frames.currentTab)
+
+    const dispatch = useDispatch()
+
+    const handleChangeTab = (id) => {
+        dispatch(setCurrentTab(id))
+    }
+
+    const handleAddTab = () => {
+        if(listFrames.length<=0){
+            dispatch(addFrame({id:1, title: "New Request"}))
+            return
+        }
+
+        let counter = listFrames[listFrames.length-1].id
+
+        dispatch(addFrame({ id: counter+1, title: "New Request" }))
+
+    }
+
+    const handleRemoveTab=(id)=> {
+        dispatch(removeFrame(id))
+    }
 
 
-export function FrameTabs({elements}){
+    return (
 
-    return(
         <div>
-            {
-                elements.map(item => 
-                    <>
-                        <div className="container-head">
+            <div className="container-head">
+                {
+                    elements.map(item =>
+                        <div>
                             <div className="div-tabs">
-                                <p>{item.title}</p>
+                                <p onClick={()=>handleChangeTab(item.id)}>{item.title}</p>
+                                <img onClick={()=>handleRemoveTab(item.id)} src={closeImg} />
                             </div>
                         </div>
-                        <div>
+                    )
+                }
+                <div>
+                    <img onClick={handleAddTab} src={addImg} />
+                </div>
+            </div>
+            <div>
+                {
+                    elements.map(item =>
+                        <div className={tabSelected !== item.id && 'tab-close'}>
                             <RequesLayout />
                         </div>
-                    </>
-                )
-            }
+                    )
+                }
+            </div>
+
         </div>
     )
 
