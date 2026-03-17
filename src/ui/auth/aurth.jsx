fe1eabf7bb4380964e0bba1b80eb563236d95c9f
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-17 13:43:35 -0600

    feat: Se agregaron algunos elementos para el manejo de los componentes de autorizacion

--- src/ui/auth/aurth.jsx
+++ src/ui/auth/aurth.jsx
@@ -1,10 +1,24 @@
+import { Select } from "../select/select"
+import { TextField } from "../textField/textField"
 
 
-export function Auth(){
+export function Auth({auth,authType,setAuth, setAuthType}){
+
+    const authElements = [
+        {
+            value: "bearer",
+            title: "Bearer Token"
+        }
+    ]
 
     return (
         <div>
-            <p>Auth</p>
+            <div>
+                <Select target={authType} elements={authElements} handleChange={(event) => setAuthType(event.target.value)} />
+            </div>
+            <div>
+                <TextField target={auth} handleTarget={(event) => setAuth(event.target.value)} />
+            </div>
         </div>
     )
 }
