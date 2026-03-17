b517f47489c3305eaab692b7713a83eff3b13a8f
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-17 12:33:50 -0600

    feat: Se implementaron mas valores para el valor inicial del slice, se implementan reducers para la actualizacion de los elementos de un frame

--- src/store/frameSlice.js
+++ src/store/frameSlice.js
@@ -4,7 +4,13 @@ const initialState = {
     listFrames: [
         {
             id: 1,
-            title: "New Request"
+            title: "New Request",
+            url: "",
+            method: "GET",
+            body: "",
+            params: [],
+            auth: null,
+            response: null,
         }
     ],
     currentTab: 1,
@@ -14,14 +20,42 @@ const frameSlice = createSlice({
     name: 'frames',
     initialState,
     reducers: {
-        addFrame(state, action){
+        addFrame(state, action) {
             state.listFrames.push(action.payload)
         },
-        setCurrentTab(state,action){
+        setCurrentTab(state, action) {
             state.currentTab = action.payload
         },
-        removeFrame(state,action){
+        removeFrame(state, action) {
             state.listFrames = state.listFrames.filter(element => element.id !== action.payload)
+        },
+        setUrl(state, action) {
+            const index = state.listFrames.findIndex(element => element.id === action.payload.id)
+
+            if (index !== -1) {
+                state.listFrames[index].url = action.payload.url
+            }
+        },
+        setMethod(state, action) {
+            const index = state.listFrames.findIndex(element => element.id === action.payload.id)
+
+            if (index !== -1) {
+                state.listFrames[index].method = action.payload.method
+            }
+        },
+        setBody(state, action) {
+            const index = state.listFrames.findIndex(element => element.id === action.payload.id)
+
+            if (index !== -1) {
+                state.listFrames[index].body = action.payload.body
+            }
+        },
+        setResponse(state, action) {
+            const index = state.listFrames.findIndex(element => element.id === action.payload.id)
+
+            if (index !== -1) {
+                state.listFrames[index].response = action.payload.response
+            }
         }
     }
 })
@@ -29,7 +63,11 @@ const frameSlice = createSlice({
 export const {
     addFrame,
     setCurrentTab,
-    removeFrame
+    removeFrame,
+    setUrl,
+    setMethod,
+    setBody,
+    setResponse
 } = frameSlice.actions
 
 export default frameSlice.reducer
