a34332b2aa4301894cacaa3f48f61a10812dde8c
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-16 22:02:15 -0600

    feat: Se implementaron los metodos para agregear un frame, remover un frame y seleccionar un frame

--- src/store/frameSlice.js
+++ src/store/frameSlice.js
@@ -3,9 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     listFrames: [
         {
+            id: 1,
             title: "New Request"
         }
-    ]
+    ],
+    currentTab: 1,
 }
 
 const frameSlice = createSlice({
@@ -14,12 +16,20 @@ const frameSlice = createSlice({
     reducers: {
         addFrame(state, action){
             state.listFrames.push(action.payload)
+        },
+        setCurrentTab(state,action){
+            state.currentTab = action.payload
+        },
+        removeFrame(state,action){
+            state.listFrames = state.listFrames.filter(element => element.id !== action.payload)
         }
     }
 })
 
 export const {
     addFrame,
+    setCurrentTab,
+    removeFrame
 } = frameSlice.actions
 
 export default frameSlice.reducer
