4483dad8a62fa3cbd8f1759f23e272f4e067d4ea
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-17 16:53:33 -0600

    feat: Se agrego la funcion para los componentes de auth, se agregan mejoras de estilos para un mejor acomodo visual

--- src/ui/auth/auth.jsx
+++ src/ui/auth/auth.jsx
@@ -1,8 +1,9 @@
+import { Box } from "../containers/containers"
 import { Select } from "../select/select"
 import { TextField } from "../textField/textField"
 
 
-export function Auth({auth,authType,setAuth, setAuthType}){
+export function Auth({ auth, authType, setAuth, setAuthType }) {
 
     const authElements = [
         {
@@ -12,13 +13,29 @@ export function Auth({auth,authType,setAuth, setAuthType}){
     ]
 
     return (
-        <div>
-            <div>
-                <Select target={authType} elements={authElements} handleChange={(event) => setAuthType(event.target.value)} />
-            </div>
-            <div>
-                <TextField target={auth} handleTarget={(event) => setAuth(event.target.value)} />
-            </div>
-        </div>
+        <Box styles={{
+            display: "flex",
+            flexDirection: "row",
+        }}>
+            <Box styles={{
+                width: "20%",
+                margin: ".2rem"
+            }}>
+                <Select 
+                    target={authType} 
+                    elements={authElements} 
+                    handleChange={(event) => setAuthType(event.target.value)} 
+                />
+            </Box>
+            <Box styles={{
+                width: "50%",
+                margin: ".2rem"
+            }}>
+                <TextField 
+                    target={auth} 
+                    handleTarget={(event) => setAuth(event.target.value)} 
+                />
+            </Box>
+        </Box>
     )
 }
