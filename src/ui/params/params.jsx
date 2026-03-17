c5eba329b1c29ca2af7c4a898d8d5ee82986fcb7
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-17 16:54:18 -0600

    feat: Se implemento la funcion para la logica de los campos, se implementan mejoras para un mejor acomodo visual

--- src/ui/params/params.jsx
+++ src/ui/params/params.jsx
@@ -1,24 +1,58 @@
+import { useDispatch, useSelector } from "react-redux";
 import { TextField } from "../textField/textField";
+import { setParamName, setParamValue } from "../../store/frameSlice";
+import { Box } from "../containers/containers";
 
 
 export function Params({elements,setElements}){
+
+    const frameId = useSelector((state) => state.frames.currentTab)
+    const dispatch = useDispatch()
+
+    const handleName=(value,paramIndex) => {
+        dispatch(setParamName({frameId: frameId, paramIndex: paramIndex, paramName: value  }))
+    }
+
+    const handleValue=(value, paramIndex) => {
+        dispatch(setParamValue({frameId: frameId, paramIndex: paramIndex, paramValue: value}))
+    }
+
     return(
         <div>
             <p>Query parameters</p>
-            <div>
+            <Box styles={{
+                display: "flex",
+                flexDirection: "row"
+            }}>
                 <p>Add</p>
                 <p>Delete All</p>
-            </div>
-            <div>
+            </Box>
+            <Box styles={{
+                display: "flex",
+                flexDirection: "column"
+            }}>
                 {
                     elements.map(item => 
-                        <div>
-                            <TextField target={item.name} />
-                            <TextField target={item.value} />
-                        </div>
+                        <Box styles={{
+                            display: "flex",
+                            flexDirection: "row"
+                        }}>
+                            <Box styles={{
+                                width: "40%",
+                                margin: "1rem"
+                            }}>
+                                <TextField target={item.name} handleTarget={(event) => handleName(event.target.value, item.id)}  />
+                            </Box>
+                            <Box styles={{
+                                width: "40%",
+                                margin: "1rem"
+                            }}>
+                                <TextField target={item.value} handleTarget={(event) => handleValue(event.target.vaslue, item.id)} />
+                            </Box>
+                        </Box>
                     )
                 }
-            </div>
+            </Box>
         </div>
     )
 }
