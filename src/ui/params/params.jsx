e5f7ae2cbc8f4298c761d184cb39feea468ab187
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-17 13:24:23 -0600

    feat: Se implemento la arquitectura de los componentes en los params

--- src/ui/params/params.jsx
+++ src/ui/params/params.jsx
@@ -1,9 +1,24 @@
+import { TextField } from "../textField/textField";
 
 
-export function Params(){
+export function Params({elements,setElements}){
     return(
         <div>
-            <p>params</p>
+            <p>Query parameters</p>
+            <div>
+                <p>Add</p>
+                <p>Delete All</p>
+            </div>
+            <div>
+                {
+                    elements.map(item => 
+                        <div>
+                            <TextField target={item.name} />
+                            <TextField target={item.value} />
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
