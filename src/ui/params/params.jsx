2fc84bd6663321bdab4347f8f741ee3e2f03f825
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-23 13:03:14 -0600

    feat: Se implemento la funcion para agregar mas parametros

--- src/ui/params/params.jsx
+++ src/ui/params/params.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { TextField } from "../textField/textField";
-import { setParamName, setParamValue } from "../../store/frameSlice";
+import { addParam, setParamName, setParamValue } from "../../store/frameSlice";
 import { Box } from "../containers/containers";
 import './params.css'
 import addImg from '../../assets/add.svg'
@@ -9,6 +9,7 @@ export function Params({elements}){
 
     const frameId = useSelector((state) => state.frames.currentTab)
     const dispatch = useDispatch()
+    const listFrames = useSelector((state) => state.frames.listFrames)
 
     const handleName=(value,paramId) => {
         dispatch(setParamName({frameId: frameId, paramId: paramId, paramName: value  }))
@@ -18,6 +19,20 @@ export function Params({elements}){
         dispatch(setParamValue({frameId: frameId, paramId: paramId, paramValue: value}))
     }
 
+    const handleAddParam=() => {
+        const frame = listFrames.find((element) => element.id == frameId)
+
+        if(frame.params.length <= 0){
+            dispatch(addParam({id: frameId,param: {id: 1, name: "", value: ""}}))
+            return
+        }
+
+        let counter = frame.params[frame.params.length - 1].id
+
+        dispatch(addParam({id: frameId, param: {id: counter + 1, name: "", value: ""}}))
+        
+    }
+
     return(
         <div>
             <p>Query parameters</p>
@@ -25,7 +40,7 @@ export function Params({elements}){
                 display: "flex",
                 flexDirection: "row"
             }}>
-                <div className="div-add">
+                <div onClick={() => handleAddParam()} className="div-add">
                     <p className="param-btn">Add</p>
                     <img src={addImg} />
                 </div>
@@ -47,13 +62,13 @@ export function Params({elements}){
                                 width: "40%",
                                 margin: "1rem"
                             }}>
-                                <TextField target={item.name} handleTarget={(event) => handleName(event.target.value, item.id)}  />
+                                <TextField textHolder={"name"} target={item.name} handleTarget={(event) => handleName(event.target.value, item.id)}  />
                             </Box>
                             <Box styles={{
                                 width: "40%",
                                 margin: "1rem"
                             }}>
-                                <TextField target={item.value} handleTarget={(event) => handleValue(event.target.value, item.id)} />
+                                <TextField textHolder={"value"} target={item.value} handleTarget={(event) => handleValue(event.target.value, item.id)} />
                             </Box>
                         </Box>
                     )
