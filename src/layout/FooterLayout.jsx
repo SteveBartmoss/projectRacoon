015e4763e729afec1bd83c8361fc60fdb69ca7e2
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-24 13:38:00 -0600

    feat: Se implemento una layput para el footer de la aplicacion

---
+++ src/layout/FooterLayout.jsx
@@ -0,0 +1,12 @@
+import { Box, Row } from "../ui/containers/containers";
+import { Footer } from "../ui/footer/footer";
+
+export function FooterLayout() {
+
+    return (
+        <Row>
+            <Footer />
+        </Row>
+    )
+
+}
