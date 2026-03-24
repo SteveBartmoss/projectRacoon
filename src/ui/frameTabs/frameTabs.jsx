c3c9a249794eaa6c1492b18639cb83e269bb2ced
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-24 14:48:07 -0600

    feat: Se implemento la propiedad key

--- src/ui/frameTabs/frameTabs.jsx
+++ src/ui/frameTabs/frameTabs.jsx
@@ -78,7 +78,7 @@ export function FrameTabs({ elements }) {
             <div className="container-head">
                 {
                     elements.map(item =>
-                        <div className="div-tabs">
+                        <div key={item.id} className="div-tabs">
                             <Chip text={item.method} type={getColor(item.method)} />
                             <p className="tab" onClick={() => handleChangeTab(item.id)}>{item.title}</p>
                             <img className="img-close" onClick={() => handleRemoveTab(item.id)} src={closeImg} />
@@ -91,7 +91,7 @@ export function FrameTabs({ elements }) {
             </div>
             {
                 elements.map(item =>
-                    <div className={tabSelected !== item.id ? 'tab-close' : 'div-content-tab'}>
+                    <div key={item.id} className={tabSelected !== item.id ? 'tab-close' : 'div-content-tab'}>
                         <RequesLayout id={item.id} />
                     </div>
                 )
