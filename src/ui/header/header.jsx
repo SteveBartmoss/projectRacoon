3ff301c1f4373280353aad04b22ac0689feccb40
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-03-31 20:52:14 -0600

    feat: Se remieron elementos que no son necesarios por ahora

--- src/ui/header/header.jsx
+++ src/ui/header/header.jsx
@@ -1,16 +1,19 @@
 import logo from "../../assets/racoon.png"
+import { Box } from "../containers/containers"
 import './header.css'
 
-export function Header(){
+export function Header() {
 
-    return(
+    return (
         <>
-            <div>
-                <img className="logo" src={logo} />
-            </div>
-            <div>
-                <h1>Racoon</h1>
-            </div>
+            <Box styles={{
+                display: "flex",
+                flexDirection: "row",
+            }}>
+                <div>
+                    <img className="logo" src={logo} />
+                </div>
+            </Box>
         </>
     )
 }
