4653892c945cf6d547e2814c390a4920eb95fe75
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-02 11:11:58 -0600

    feat: Se implementaron componentes verticales y horizontales para el componente box

--- src/ui/containers/containers.jsx
+++ src/ui/containers/containers.jsx
@@ -26,3 +26,21 @@ export function Box({styles,children}){
         </div>
     )
 }
+
+export function HBox({styles, children}){
+
+    return (
+        <div className="h-box" style={styles}>
+            {children}
+        </div>
+    )
+}
+
+export function VBox({styles, children}){
+
+    return(
+        <div className="v-box" style={styles}>
+            {children}
+        </div>
+    )
+}
