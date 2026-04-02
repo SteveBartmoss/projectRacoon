af5085651e0f31b3f5ab3aa351c4e716c77e09f2
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-02 11:19:17 -0600

    feat: Se removieron los componentes row y column

--- src/ui/containers/containers.jsx
+++ src/ui/containers/containers.jsx
@@ -1,23 +1,5 @@
 import "./container.css";
 
-export function Row({children}){
-
-    return(
-        <div className="div-row">
-            {children}
-        </div>
-    )
-}
-
-export function Column({children}){
-
-    return(
-        <div className="div-col">
-            {children}
-        </div>
-    )
-}
-
 export function Box({styles,children}){
 
     return(
