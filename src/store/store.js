923b74d81369d50f15809a6dcc7f4c3aebde0116
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-05 19:24:12 -0600

    feat: Se implementaron dos nuevos slices para la division de la ui y la informacion que se representa

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import frameReducer from "./frameSlice"
+import tabSlice from "./tabSlice";
+import requestSlice from "./requestSlice"
 
 export const store = configureStore({
     reducer: {
         frames: frameReducer,
+        tabs: tabSlice,
+        requests: requestSlice
     }
 })
