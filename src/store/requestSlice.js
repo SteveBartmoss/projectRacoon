91246f79946af6e62242de23cb89a62aad9f2857
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-05 19:24:39 -0600

    feat: Se implementa un estado normalizado para las requests

---
+++ src/store/requestSlice.js
@@ -0,0 +1,153 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    requestsById: {
+
+    },
+    requestIds: [],
+    counter: 1,
+}
+
+const getRequest = (state, id) => {
+    return state.requestsById[id]
+}
+
+const getParam = (request, id) => {
+    return request.paramsById[id]
+}
+
+const requestSlice = createSlice({
+    name: 'requests',
+    initialState,
+    reducers: {
+        addRequest(state, action) {
+            const request = action.payload
+
+            state.requestIds[request.id] = request
+            state.requestIds.push(frame.id)
+        },
+        removeFrame(state, action) {
+            const id = action.payload
+
+            delete state.requestsById[id]
+            state.requestIds = state.requestIds.filter(element => element !== id)
+        },
+        setUrl(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.url = action.payload.url
+            }
+        },
+        setMethod(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.method = action.payload.method
+            }
+        },
+        setBody(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.body = action.payload.body
+            }
+        },
+        setResponse(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.response = action.payload.response
+            }
+        },
+        setAuth(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.auth = action.payload.auth
+            }
+        },
+        setAuthType(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.authType = action.payload.authType
+            }
+        },
+        setParamName(state, action) {
+
+            const request = getRequest(state, action.payload.requestId)
+
+            if (!request) return
+
+            const param = getParam(request, action.payload.paramId)
+
+            if (param) {
+                param.name = action.payload.paramName
+            }
+        },
+        setParamValue(state, action) {
+
+            const request = getRequest(state, action.payload.requestId)
+
+            if (!request) return
+
+            const param = getParam(request, action.payload.paramId)
+
+            if (param) {
+                param.value = action.payload.paramValue
+            }
+        },
+        addParam(state, action) {
+
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.paramsById[action.payload.param.id] = action.payload.param
+                request.paramIds.push(action.payload.param.id)
+            }
+        },
+        removeParam(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                delete request.paramsById[action.payload.paramId]
+                request.paramIds = request.paramIds.fitler(element => element !== action.payload.paramId)
+            }
+        },
+        cleanParams(state, action) {
+            const request = getRequest(state, action.payload.id)
+
+            if (request) {
+                request.paramsById = {
+                    1: {
+                        id: 1,
+                        name: "",
+                        value: "",
+                    }
+                }
+                request.paramIds = [1]
+            }
+
+            state.counter = 1
+        }
+    }
+})
+
+export const {
+    addRequest,
+    removeFrame,
+    setUrl,
+    setMethod,
+    setBody,
+    setResponse,
+    setAuth,
+    setAuthType,
+    setParamName,
+    setParamValue,
+    addParam,
+    removeParamm,
+    cleanParams
+} = requestSlice.actions
+
+export default requestSlice.reducer
