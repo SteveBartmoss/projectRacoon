8188190edd1eedc14264e1cef4bbf0a57d3968b5
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-11 17:40:41 -0600

    feat: Se implemento el nuevo slice para los errores

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import frameReducer from "./frameSlice"
 import tabSlice from "./tabSlice";
 import requestSlice from "./requestSlice"
+import errorSlice from "./errorsSlice"
 
 export const store = configureStore({
     reducer: {
         frames: frameReducer,
         tabs: tabSlice,
-        requests: requestSlice
+        requests: requestSlice,
+        errors: errorSlice,
     }
 })
