f98e6db1b87159233cb2052e5fb2215ca4ff3f9b
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-11 17:41:03 -0600

    feat: Se implemento el estado de los contadores

--- src/ui/appErrors/AppErrors.jsx
+++ src/ui/appErrors/AppErrors.jsx
@@ -1,18 +1,22 @@
 import './appErrors.css'
 import errorImg from '../../assets/dangerous.svg'
 import warningImg from '../../assets/warning.svg'
+import { useSelector } from 'react-redux'
 
 export function AppErrors(){
+
+    const errors = useSelector((state) => state.errors.errorCounter)
+    const warnings = useSelector((state) => state.errors.warningCounter)
 
     return(
         <div className='div-errors'>
             <div className='div-element'>
                  <img className='icon' src={errorImg} />
-                <p>0</p>
+                <p>{errors}</p>
             </div>
             <div className='div-element'>
                 <img className='icon' src={warningImg} />
-                <p>0</p>
+                <p>{warnings}</p>
             </div>
         </div>
     )
