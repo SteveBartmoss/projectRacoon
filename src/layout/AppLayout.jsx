03782317f3416365879aec467ae4b77c21866a5c
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-11 20:47:15 -0600

    feat: Se implemento el nuevo componente de layout para los widgets

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import { Box } from "../ui/containers/containers"
 import { Header } from "../ui/header/header"
 import { FrameTabs } from "../ui/frameTabs/frameTabs"
 import { FooterLayout } from "./FooterLayout"
-import { ToggleWindow } from "../ui/toggleWindow/toggleWindow"
+import { WidgetsLayout } from "./WidgetsLayout"
 
 export function AppLayout() {
 
@@ -21,7 +21,7 @@ export function AppLayout() {
             }}>
                 <Header />
                 <FrameTabs elements={tabs} />
-                <ToggleWindow />
+                <WidgetsLayout />
                 <FooterLayout />
             </Box>
         </>
