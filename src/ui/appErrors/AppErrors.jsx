7243e2f5e7ea6df40ce1db9eafcaec87744fbee2
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-11 19:36:49 -0600

    feat: Se implemento la funcion para mostrar la ventana de los errores

--- src/ui/appErrors/AppErrors.jsx
+++ src/ui/appErrors/AppErrors.jsx
@@ -1,15 +1,23 @@
 import './appErrors.css'
 import errorImg from '../../assets/dangerous.svg'
 import warningImg from '../../assets/warning.svg'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setShowErrorWindow } from '../../store/appSlice'
 
 export function AppErrors(){
 
     const errors = useSelector((state) => state.errors.errorCounter)
     const warnings = useSelector((state) => state.errors.warningCounter)
+    const dispatch = useDispatch()
+    const showError = useSelector((state)=> state.appInfo.showErrorsWindow)
+
+    const handleShowErrors = () => {
+        dispatch(setShowErrorWindow({value: !showError}))
+        console.log(showError)
+    }
 
     return(
-        <div className='div-errors'>
+        <div onClick={handleShowErrors} className='div-errors'>
             <div className='div-element'>
                  <img className='icon' src={errorImg} />
                 <p>{errors}</p>
