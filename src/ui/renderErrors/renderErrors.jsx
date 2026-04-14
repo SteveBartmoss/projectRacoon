2f28ef791438316286e0eb4b661d4810657a6629
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-04-13 21:26:16 -0600

    feat: Se implementaron estilos para el componente que dibuja los errores

--- src/ui/renderErrors/renderErrors.jsx
+++ src/ui/renderErrors/renderErrors.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Box } from "../containers/containers";
-
+import './renderErrors.css'
 
 export function RenderErrors() {
 
@@ -10,12 +10,13 @@ export function RenderErrors() {
         <Box styles={{
             display: "flex",
             flexDirection: "column",
-            overflowY: "scroll",
+            maxHeight: "100%",
+            overflowY: "scroll",            
         }}
         >
             {
                 messsages.map(item =>
-                    <pre>
+                    <pre className="pre-error">
                         {JSON.stringify(item, null, 2)}
                     </pre>
                 )
