2b7ff1c64e5069c8b96b41b4aefc0147c8b9b4c1
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-01 10:21:45 -0600

    feat: Se implemento una funcion para regresar un objeto de reques vacio

--- src/utils/requestUtils.js
+++ src/utils/requestUtils.js
@@ -18,3 +18,35 @@ export function loadRequest(id, title, json) {
         description: json.description
     }
 }
+
+export function loadEmptyRequest(id) {
+    return {
+        id: id,
+        title: "New Request",
+        url: "",
+        method: "GET",
+        body: "",
+        paramsById: {
+            1: {
+                id: 1,
+                name: "",
+                value: "",
+                active: true,
+            },
+        },
+        paramIds: [1],
+        headersById: {
+            1: {
+                id: 1,
+                name: "",
+                value: "",
+                active: true,
+            }
+        },
+        headerIds: [1],
+        auth: "",
+        authType: "",
+        response: {},
+        description: ""
+    }
+}
