48e9433ffee3e3acce4fedb95046fda6ca546d03
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-01 12:09:46 -0600

    feat: Se removio un slice que ya no se usa

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import frameReducer from "./frameSlice"
 import tabSlice from "./tabSlice";
 import requestSlice from "./requestSlice"
 import errorSlice from "./errorsSlice"
@@ -7,7 +6,6 @@ import appSlice from "./appSlice"
 
 export const store = configureStore({
     reducer: {
-        frames: frameReducer,
         tabs: tabSlice,
         requests: requestSlice,
         errors: errorSlice,
