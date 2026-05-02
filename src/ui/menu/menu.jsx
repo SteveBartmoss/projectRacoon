a110d986968c2605a2f0701072ca86133133c7b5
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-02 11:29:38 -0600

    feat: Se cambio la implementacion de abrir el menu, ahora se habre al hacer click en el menu y se cierra al hacer click fuera del menu

--- src/ui/menu/menu.jsx
+++ src/ui/menu/menu.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import './menu.css'
 
 export function Menu({ elements, title }) {
 
     const [show, setShow] = useState(false)
+    const menuRef = useRef(null)
 
     const handleOpen = () => {
         setShow(true)
@@ -13,10 +14,26 @@ export function Menu({ elements, title }) {
         setShow(false)
     }
 
+    useEffect(()=> {
+        const handleClickOutside =  (e) => {
+            if(menuRef.current && !menuRef.current.contains(e.target)){
+                handleClose()
+            }
+        }
+
+        if(show){
+            document.addEventListener('mousedown', handleClickOutside)
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    },[show])
+
     return (
-        <div className="display-menu" onMouseEnter={handleOpen}>
-            <div className="display-menu-title">{title}</div>
-            <div onMouseLeave={handleClose}  className={show ? 'display-menu-list' : 'display-menu-none'}>
+        <div className="display-menu" ref={menuRef}>
+            <div onClick={handleOpen} className="display-menu-title">{title}</div>
+            <div className={show ? 'display-menu-list' : 'display-menu-none'}>
                 {
                     elements.map(item =>
                         <div className="display-menu-item" onClick={item.action}>
