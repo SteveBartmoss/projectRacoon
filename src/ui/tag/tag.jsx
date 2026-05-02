c6a1bca3f6afb3f0418f3319244c56b1e3d92216
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-02 12:14:17 -0600

    feat: Se incremento la version de la aplicacion

--- src/ui/tag/tag.jsx
+++ src/ui/tag/tag.jsx
@@ -18,7 +18,7 @@ export function GitTag() {
                 target="_blank"
                 rel="noreferrer"
             >
-                1.13.2
+                1.13.4
             </a>
         </div>
     )
