70ed9eb2aff88babb7eb7825f8289ba5f5ccae8e
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-10 14:45:38 -0600

    feat: Se implementa la recuperacion de la configuracion de la aplicacion desde el inicio de la aplicacion

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,15 +1,34 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Box } from "../ui/containers/containers"
 import { Header } from "../ui/header/header"
 import { FrameTabs } from "../ui/frameTabs/frameTabs"
 import { FooterLayout } from "./FooterLayout"
 import { ResizeLayout, WidgetsLayout } from "./WidgetsLayout"
+import { invoke } from "@tauri-apps/api/core"
+import { useEffect } from "react"
+import { setConfig } from "../store/appSlice"
 
 export function AppLayout() {
 
     const tabs = useSelector((state) => 
         state.tabs.tabIds.map(id => state.tabs.tabsById[id])
     )
+
+    const dispatch = useDispatch()
+
+    const initConfig = async () => {
+        const config = await invoke("load_config");
+
+        console.log(config)
+
+        dispatch(setConfig(config))
+
+        return
+    }
+
+    useEffect(() => {
+        initConfig()
+    },[]);
 
     return (
         <>
