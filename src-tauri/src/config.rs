901ef5ae183bb633689365176b042e25d9388472
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-10 14:44:19 -0600

    feat: Se implementa una funcion para guardar la configuracion de la aplicacion

---
+++ src-tauri/src/config.rs
@@ -0,0 +1,48 @@
+use std::fs;
+use std::path::PathBuf;
+use serde::{Serialize, Deserialize};
+use tauri::Manager;
+
+#[derive(Serialize, Deserialize)]
+pub struct AppConfig {
+    theme: String,
+    path: String,
+    token: String,
+}
+
+#[tauri::command]
+pub fn load_config(app: tauri::AppHandle) -> Result<AppConfig, String>{
+
+    let config_dir = app
+        .path()
+        .app_config_dir()
+        .map_err(|e| e.to_string())?;
+
+    fs::create_dir_all(&config_dir).unwrap();
+
+    let config_path = config_dir.join("config.json");
+
+    if(!config_path.exists()){
+
+        let default = AppConfig{
+            theme: "dark".into(),
+            path: "".into(),
+            token: "".into(),
+        };
+
+        let json = serde_json::to_string_pretty(&default).unwrap();
+        fs::write(&config_path,json).unwrap();
+
+        return Ok(default)
+    }
+
+    let content = fs::read_to_string(config_path)
+        .map_err(|e| e.to_string())?;
+
+    let config: AppConfig = 
+        serde_json::from_str(&content)
+        .map_err(|e| e.to_string())?;
+
+    Ok(config)
+
+}
