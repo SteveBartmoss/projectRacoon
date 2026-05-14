d06cce5f371ef4d4673f65d6e469954548499f70
Marco <za19012397@zapopan.tecmm.edu.mx>
2026-05-13 18:15:23 -0600

    feat: Se agrego la funcion para guardar una request desde un comando

--- src/ui/comandObserver/comandObserver.jsx
+++ src/ui/comandObserver/comandObserver.jsx
@@ -1,8 +1,19 @@
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { save } from "@tauri-apps/plugin-dialog";
+import { writeFile } from "@tauri-apps/plugin-fs";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setInfo } from "../../store/requestSlice";
 
 
 export function ComandObserver({ children }) {
+
+    const tabSelected = useSelector((state) => state.tabs.currentTab)
+    const request = useSelector((state) => state.requests.requestsById[tabSelected])
+    const listFrames = useSelector((state) => state.tabs.tabIds)
+    const tabCounter = useSelector((state) => state.tabs.counter)
+
+    const dispatch = useDispatch()
 
     const handleNewWindow = () => {
         const webview = new WebviewWindow(`window-${Date.now()}`, {
@@ -11,6 +22,29 @@ export function ComandObserver({ children }) {
             width: 800,
             height: 600,
         })
+    }
+
+    const handleSaveTab = async () => {
+
+        const path = await save({
+            defaultPath: "newrequest.json"
+        })
+
+        if(!path) return
+
+        const encoder = new TextEncoder()
+
+        const bytes = encoder.encode(
+            JSON.stringify(request, null, 2)
+        )
+
+        await writeFile(path, bytes)
+
+        dispatch(setInfo({
+            id: tabSelected,
+            field: "path",
+            value: path
+        }))
     }
 
     useEffect(() => {
@@ -28,7 +62,7 @@ export function ComandObserver({ children }) {
 
             if (event.ctrlKey && event.code === "KeyS") {
                 event.preventDefault();
-                console.log("Guardar");
+                handleSaveTab()
             }
 
             if(event.ctrlKey && event.code == "KeyN"){
