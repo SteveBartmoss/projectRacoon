77ae79c22731e0df21ff9ed2ce7c0a5fcc4c94b8
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-06-20 15:12:02 -0600

    feat: Se implemento que si el contenido es una archivo html, se detecte y se renderice en lugar de mostrarse como json

--- src/ui/responseFrame/responseFrame.jsx
+++ src/ui/responseFrame/responseFrame.jsx
@@ -17,23 +17,23 @@ export function ResponseFrame({ objProps }) {
     const dispatch = useDispatch()
 
     const handleClipBoard = async () => {
-        try{
+        try {
 
             await navigator.clipboard.writeText(JSON.stringify(objProps.body))
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     }
 
     const setStatusResponse = (status) => {
 
-        if(!status ) return 'main'
+        if (!status) return 'main'
 
-        if(status >= 200 && status < 300) return 'success'
-        if(status >= 300 && status < 400) return 'main'
-        if(status >= 400 && status < 500) return 'warning'
-        if(status >= 500) return 'error'
+        if (status >= 200 && status < 300) return 'success'
+        if (status >= 300 && status < 400) return 'main'
+        if (status >= 400 && status < 500) return 'warning'
+        if (status >= 500) return 'error'
 
         return 'main'
     }
@@ -44,7 +44,7 @@ export function ResponseFrame({ objProps }) {
             defaultPath: "response.json"
         })
 
-        if(!path) return
+        if (!path) return
 
         const encoder = new TextEncoder()
 
@@ -52,8 +52,8 @@ export function ResponseFrame({ objProps }) {
             JSON.stringify(objProps.body, null, 2)
         )
 
-        await writeFile(path,bytes)
-        
+        await writeFile(path, bytes)
+
     }
 
     const cleanResponse = async () => {
@@ -78,7 +78,7 @@ export function ResponseFrame({ objProps }) {
                             margin: ".3rem",
                         }}
                     >
-                        <Chip text={`status: ${objProps?.status ?? ""}`} type={setStatusResponse(objProps?.status)}/>
+                        <Chip text={`status: ${objProps?.status ?? ""}`} type={setStatusResponse(objProps?.status)} />
                     </Box>
                     <Box
                         styles={{
@@ -88,7 +88,7 @@ export function ResponseFrame({ objProps }) {
                             margin: ".3rem",
                         }}
                     >
-                        <Chip text={`time: ${objProps?.time ?? ""}`} type={'main'}/>
+                        <Chip text={`time: ${objProps?.time ?? ""}`} type={'main'} />
                     </Box>
                     <Box
                         styles={{
@@ -135,7 +135,21 @@ export function ResponseFrame({ objProps }) {
                     </Box>
                 </div>
                 <div className='div-response'>
-                    <pre>{JSON.stringify(objProps.body, null, 2)}</pre>
+                    {objProps.typeBody === 'Html' ? (
+                        <iframe
+                            srcDoc={objProps.body}
+                            style={{
+                                width: '100%',
+                                height: '500px',
+                                border: 'none'
+                            }}
+                        />
+                    ) : objProps.typeBody === 'Json' ? (
+                        <pre>{JSON.stringify(objProps.body, null, 2)}</pre>
+                    ) : (
+                        <pre>{objProps.body}</pre>
+                    )
+                    }
                 </div>
             </div>
         </>
