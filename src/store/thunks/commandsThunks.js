12c8e76915ec68f8bc78a372ce52ea74d4e98f6d
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-06-20 19:27:00 -0600

    feat: Se corije el nombre y la importacion para la funcion createTabFromJson, se implementa la nueva funcion para cargar una request y una response desde un archivo json

--- src/store/thunks/commandsThunks.js
+++ src/store/thunks/commandsThunks.js
@@ -1,24 +1,27 @@
 import { open, save } from "@tauri-apps/plugin-dialog"
 import { readFile, writeFile } from "@tauri-apps/plugin-fs"
 import { setInfo } from "../requestSlice"
-import { loadEmptyRequest, loadRequest } from "../../utils/requestUtils"
+import { builJson2Donwload, loadEmptyRequest } from "../../utils/requestUtils"
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow"
-import { createNewTab,crerateTabFromJson } from "./tabsManagerThunks"
+import { createNewTab,createTabFromJson } from "./tabsManagerThunks"
 
 
 export const saveCurrentTab = () => async (dispatch, getState) => {
 
-    const { tabs, requests } = getState()
+    const { tabs, requests, responses } = getState()
     const tabId = tabs.currentTab
     const request = requests.requestById[tabId]
+    const response = responses.responsesById[tabId]
 
     if (!request) return
 
     const path = await save({ defaultPath: 'newrequest.json' })
     if (!path) return
 
+    const objDonwload = builJson2Donwload(request,response)
+
     const encoder = new TextEncoder()
-    await writeFile(path, encoder.encode(JSON.stringify(request, null, 2)))
+    await writeFile(path, encoder.encode(JSON.stringify(objDonwload, null, 2)))
 
     dispatch(setInfo({ id: tabId, field: 'path', value: path }))
 
@@ -37,7 +40,7 @@ export const openFileAsNewTab = () => async (dispatch, getState) => {
     const decoder = new TextDecoder()
     const json = JSON.parse(decoder.decode(bytes))
 
-    await dispatch(crerateTabFromJson(json))
+    await dispatch(createTabFromJson(json))
 
     // Todo: manejarlo dentro de la tab que se acaba de crear
     //dispatch(setInfo({ id: newCounter, field: 'path', value: path }));
