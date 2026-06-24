538567f9df27639226cc25c30df6ed3aaf2bfce1
steve <za19012397@zapopan.tecmm.edu.mx>
2026-06-24 01:52:58 -0600

    feat: Se separa la construccion del cliente ya que el codigo principal se estaba volviendo muy largo

---
+++ src-tauri/src/client.rs
@@ -0,0 +1,27 @@
+pub fn build_client(
+    state: &AppState,
+    req: &HttpRequest,
+) -> Result<reqwest::Client, HttpError> {
+
+    if !req.follow_redirects || req.verify_tls == Some(false) {
+        let mut builder = reqwest::Client::builder()
+            .redirect(
+                if req.follow_redirects {
+                    reqwest::redirect::Policy::default()
+                } else {
+                    reqwest::redirect::Policy::none()                }
+            );
+
+        if let Some(verify) = req.verify_tls {
+            builder = builder.danger_accept_invalid_certs(!verify);
+        }
+
+        return builder
+            .build()
+            .map_err(|e| HttpError::Other(e.to_string()));
+
+    }
+
+    Ok(state.client.clone())
+
+}
