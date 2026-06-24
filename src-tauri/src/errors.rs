a0938c10860711d07fd17be8a477a2e7cd8378b9
steve <za19012397@zapopan.tecmm.edu.mx>
2026-06-24 01:55:15 -0600

    feat: Se separo el struct de los errores para no tenerlo declarado dentro del mismo codigo del reqwester

---
+++ src-tauri/src/errors.rs
@@ -0,0 +1,21 @@
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Debug, Error, Serialize)]
+pub enum HttpError {
+    #[error("Network error: {0}")]
+    Network(String),
+
+    #[error("Invalid header: {0}")]
+    HeaderParse(string),
+
+    #[error("Failed to parse JSON response: {0}")]
+    JsonParse(String),
+
+    #[error("Response body execeeded size limit of {0} bytes")]
+    ResponseTooLarge(usize),
+
+    #[error("{0}")]
+    Other(String),
+    
+}
