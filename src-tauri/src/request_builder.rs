a2eb0f13b73ac3f10b40bf0a5297e8921eb844d2
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-07-13 19:41:48 -0600

    feat: Se agregan mejoras y correciones para las nuevas funciones modulares del reqwester

--- src-tauri/src/request_builder.rs
+++ src-tauri/src/request_builder.rs
@@ -1,15 +1,18 @@
 
+use std::collections::HashMap;
 
 pub fn apply_headers(
     mut req: reqwest::RequestBuilder,
     headers: Option<&Vec<(String, String)>>
 )-> Resutl<(reqwest::RequestBuilder, bool),HttpError>{
 
-    if let Some(headers) = &req.headers {
+    let mut user_set_content_type = false;
 
-        for(key, value) in headers {
+    if let Some(hdrs) = headers {
 
-            let name: = HeaderName::from_bytes(key.trim().as_bytes())
+        for(key, value) in hdrs {
+
+            let name = HeaderName::from_bytes(key.trim().as_bytes())
                 .map_err(|e| HttpError::HeaderParse(format!("Invalid header name '{}': {}",key, e)))?;
 
             let val = HeaderValue::from_str(value.trim())
@@ -19,23 +22,25 @@ pub fn apply_headers(
                 user_set_content_type = true;
             }
 
-            request = request.header(name, val);
+            req = req.header(name, val);
 
         }
 
     }
 
-    Ok(req)
+    Ok(req, user_set_content_type)
+
 }
 
 pub fn apply_body(
-    req: reqwest::RequestBuilder,
+    mut req: reqwest::RequestBuilder,
     prepared_body: &PreparedBody,
-)-> Result<HttpRequest,HttpError>{
+    user_set_content_type: bool,
+)-> Result<reqwest::RequestBuilder,HttpError>{
 
-    if let Some(form) = prepad_body.multipart_form {
+    if let Some(form) = &prepad_body.multipart_form {
         req = req.multipart(form);
-    } else if let Soime(bytes) = prepad_body.body_bytes {
+    } else if let Some(bytes) = &prepared_body.body_bytes {
         if !user_set_content_type {
             if let Some(ct) = prepared_body.content_type {
                 req = req.header("content-type", ct);
@@ -61,13 +66,15 @@ pub fn build_request(
         HttpMethod::DELETE => client.delete(&req.url),
     };
 
-    if let Some(params) = req.params {
+    if let Some(ref params ) = req.params {
         request = request.query(&params);
     }
 
     request = apply_headers(request, req.headers.as_ref())?;
 
-    request = apply_body(request,prepared_body)?;
+    let (request, user_set_content_type) = apply_headers(request, req.headers.as_ref())?;
+
+    let request = apply_body(request,prepared_body)?;
 
     Ok(
         request.timeout(
