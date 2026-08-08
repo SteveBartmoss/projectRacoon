ee8ee1e2253f455c1c6be6daace680c35f73af0d
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-08 14:11:10 -0600

    fix: Se marca como pruebas, ya que de momento causaba problemas para la aplicacion

---
+++ src-tauri/src/models-test/mod.rs
@@ -0,0 +1,8 @@
+pub mod request_models;
+
+pub use request_models::RequestBody;
+pub use request_models::ResponseBody;
+pub use request_models::Multipart;
+pub use request_models::HttpMethod;
+pub use request_models::HttpRequest;
+pub use request_models::HttpResponse;
