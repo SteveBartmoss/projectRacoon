3c48cd02fc2ea3ed2c64e0b07aa5470bbd895bfc
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-16 09:38:01 -0600

    feat: Se implementan mejores estilos y se reutilizan componentes para mejorar la presentacion de la informacion del timeline

--- src/ui/timeline/timelineItem.jsx
+++ src/ui/timeline/timelineItem.jsx
@@ -1,28 +1,60 @@
 import './timeline.css'
 import { QExpandBox } from "../../quark-jsx/expand/QExpandBox";
+import { ResponseHeaders } from '../responseHeaders/responseHeaders';
+import { ResponseFrame } from '../responseFrame/responseFrame';
+import { Chip } from '../chip/chip';
 
 
 export function TimelineItem({item}){
 
     //{JSON.stringify(item)}
 
+    const setStatusResponse = (status) => {
+
+        if (!status) return 'main'
+
+        if (status >= 200 && status < 300) return 'success'
+        if (status >= 300 && status < 400) return 'main'
+        if (status >= 400 && status < 500) return 'warning'
+        if (status >= 500) return 'error'
+
+        return 'main'
+    }
+
     return (
         <div className='div-timeline-item'>
             <div className="title-timeline-item">
-                <h1>{item.response.status}</h1>
-                <h1>{item.response.time}</h1>
-                <h1>{item.response.size}</h1>
+                <Chip 
+                    text={`status: ${item.response.status} ?? ""`}
+                    type={setStatusResponse(item.response.status)}
+                />
+
+                <Chip
+                    text={`time: ${item.response.time}`}
+                    type={'main'} 
+                />
+                <Chip
+                    text={`size: ${item.response.size}`}
+                    type={'main'} 
+                />
             </div>
             <div>
                 <QExpandBox 
                     title="Headers"
-                    content={JSON.stringify(item.response.headers)}
+                    content={
+                        <ResponseHeaders headers={item.response.headers} />
+                    }
                 />
             </div>
             <div>
                 <QExpandBox
                     title="Body"
-                    content={JSON.stringify(item.response.body)}
+                    content={
+                        <ResponseFrame 
+                            typeBody={item.response.body.type}
+                            body={item.response.body.value}
+                        />
+                    }
                 />
             </div>
         </div>
