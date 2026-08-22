9b8170aa873b1b5b873f0c5796de4b400fe4dce9
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-08-22 12:31:29 -0600

    feat: Se implemento un componente para el manejo del data forma en la aplicacion

---
+++ src/ui/dataForm/dataForm.jsx
@@ -0,0 +1,26 @@
+
+
+export function DataForm() {
+
+    return (
+        <div>
+            <Box styles={{
+                display: "flex",
+                flexDirection: "row"
+            }}>
+                <div>
+                    <p>Add</p>
+                </div>
+            </Box>
+            <Box styles={{
+                display: "flex",
+                flexDirection: "column"
+            }}>
+                {
+                    
+                }
+            </Box>
+        </div>
+
+    )
+}
